42a3624c108fef986a7fcc3bd75ed563010acc7a
OmarOlek <52744091+OmarOlek@users.noreply.github.com>
2026-02-20 00:31:05 +0100

    Update counter_pre_exit_rate.sql

--- queries/counter_pre_exit_rate.sql
+++ queries/counter_pre_exit_rate.sql
@@ -1,42 +1,16 @@
-WITH pool_fees AS (
-    SELECT
-        pool AS pool_address,
-        fee / 1e6 AS fee_rate
-    FROM uniswap_v3_ethereum.Factory_evt_PoolCreated
+WITH burns AS (
+    SELECT DISTINCT evt_tx_from AS wallet
+    FROM uniswap_v3_ethereum.Pair_evt_Burn
+    WHERE evt_block_time >= TIMESTAMP '2025-11-03'
+      AND evt_block_time < TIMESTAMP '2025-12-27'
+),
+mints AS (
+    SELECT DISTINCT evt_tx_from AS wallet
+    FROM uniswap_v3_ethereum.Pair_evt_Mint
+    WHERE evt_block_time >= TIMESTAMP '2025-11-03'
+      AND evt_block_time < TIMESTAMP '2025-12-27'
 )
-
-SELECT
-    CASE
-        WHEN t.block_date < DATE '2025-12-27' THEN 'Pre-Switch'
-        ELSE 'Post-Switch'
-    END AS period,
-    COUNT(DISTINCT t.block_date) AS days,
-    ROUND(SUM(t.amount_usd) / COUNT(DISTINCT t.block_date), 0) AS avg_daily_volume,
-    ROUND(SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date), 0) AS avg_daily_pool_fees,
-    ROUND(
-        CASE
-            WHEN t.block_date < DATE '2025-12-27'
-            THEN SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date) * 1.0
-            ELSE SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date) * 0.85
-        END, 0
-    ) AS avg_daily_lp_earnings,
-    ROUND(SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date), 0)
-        - ROUND(
-            CASE
-                WHEN t.block_date < DATE '2025-12-27'
-                THEN SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date) * 1.0
-                ELSE SUM(t.amount_usd * p.fee_rate) / COUNT(DISTINCT t.block_date) * 0.85
-            END, 0
-        ) AS redirected_to_protocol
-FROM dex.trades t
-LEFT JOIN pool_fees p
-    ON t.project_contract_address = p.pool_address
-WHERE t.blockchain = 'ethereum'
-  AND t.project = 'uniswap'
-  AND t.version = '3'
-  AND t.block_date >= DATE '2025-11-01'
-  AND t.block_date <= DATE '2026-02-17'
-  AND t.amount_usd > 0
-  AND t.amount_usd < 1e10
-GROUP BY 1
-ORDER BY 1
+SELECT ROUND(COUNT(*) / 54.0, 1) AS exits_per_day
+FROM burns b
+LEFT JOIN mints m ON b.wallet = m.wallet
+WHERE m.wallet IS NULL
